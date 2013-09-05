ed0a3a4b2952d4a0630e6a29046ca5e6159f2c9d
delphinus <delphinus@remora.cx>
2013-09-05 23:36:24 +0900

    initial commit

---
+++ lib/Net/Moves.pm
@@ -0,0 +1,32 @@
+package Net::Moves;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Net::Moves -
+
+=head1 SYNOPSIS
+
+  use Net::Moves;
+
+=head1 DESCRIPTION
+
+Net::Moves is
+
+=head1 AUTHOR
+
+JINNOUCHI Yasushi E<lt>delphinus {at} remora.cxE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
